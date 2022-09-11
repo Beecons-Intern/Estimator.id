92751328b8d7d5403eec285ba1693704484e301e
apiiRR <ramadhanarafi437@gmail.com>
2022-09-11 16:31:59 +0700

    fixing UI

--- lib/app/screens/profile_screen/components/body.dart
+++ lib/app/screens/profile_screen/components/body.dart
@@ -72,7 +72,7 @@ class Body extends StatelessWidget {
         DraggableScrollableSheet(
           initialChildSize: 0.6,
           minChildSize: 0.6,
-          maxChildSize: 0.8,
+          maxChildSize: 0.9,
           builder: (context, controller) => ClipRRect(
             borderRadius: const BorderRadius.only(
                 topLeft: Radius.circular(20), topRight: Radius.circular(20)),
